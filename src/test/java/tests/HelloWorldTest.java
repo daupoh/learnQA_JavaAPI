ca47c0ae89321fe20267700d356c8e9699d3059b
daupoh <levievgen26@gmail.com>
2024-07-22 16:40:49 +0300

    ex13 user agent

--- src/test/java/tests/HelloWorldTest.java
+++ src/test/java/tests/HelloWorldTest.java
@@ -1,12 +1,15 @@
 package tests;
 
+import io.restassured.http.Header;
 import io.restassured.http.Headers;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import lib.Assertion;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import io.restassured.RestAssured;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashMap;
@@ -17,6 +20,39 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class HelloWorldTest {
 
+    @ParameterizedTest
+    @ValueSource (strings = {
+            "Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30" +
+                    "|Mobile|No|Android"
+            ,"Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1" +
+            "|Mobile|Chrome|iOS"
+            ,"Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)" +
+            "|Googlebot|Unknown|Unknown"
+            ,"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0" +
+            "|Web|Chrome|No"
+            ,"Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1" +
+            "|Mobile|No|iPhone"} )
+    public void testCheckUserAgent(String userAgentCaseSpec) {
+        Map <String,String> headers = new HashMap<String, String>();
+        String[] caseParams = userAgentCaseSpec.split("\\|");
+        String userAgentVal = caseParams[0],
+                platform = caseParams[1],
+                browser = caseParams[2],
+                device = caseParams[3];
+        headers.put("User-Agent",userAgentVal);
+
+        Response response = RestAssured
+                .given()
+                .headers(headers)
+                .get("https://playground.learnqa.ru/ajax/api/user_agent_check")
+                .andReturn();
+        System.out.println(String.format("Expected platform %s, browser %s and device %s",
+                platform,browser,device));
+        response.prettyPrint();
+        assertEquals(platform,response.jsonPath().getString("platform"),"Platform doesn't have expected value");
+        assertEquals(browser,response.jsonPath().getString("browser"),"Browser doesn't have expected value");
+        assertEquals(device,response.jsonPath().getString("device"),"Device doesn't have expected value");
+    }
     @Test
     public void testCheckHeadersRequest() {
         /*String[] expHeaderNames =
